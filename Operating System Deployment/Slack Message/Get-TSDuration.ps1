5dae85b80cff63691a0e960b700e2a52fe203c05
Kuntz <john.kuntz@cmworks.com>
2019-03-30 12:30:19 -0400

    Updated to use worldclock API and use file time for the start and end times. Additionally there is an attempt to get the logged in user

--- Operating System Deployment/Slack Message/Get-TSDuration.ps1
+++ Operating System Deployment/Slack Message/Get-TSDuration.ps1
@@ -10,8 +10,10 @@ $tsenv = New-Object -ComObject Microsoft.SMS.TSEnvironment
 
 # Set the Task Sequence Variable based on the parameter passed.
 if ($StartEnd -eq 'Start') {
-    $tsenv.Value('StartTime') = Get-Date
+    $tsenv.Value('StartTime') = [datetime]::FromFileTime((Invoke-RestMethod -Uri 'http://worldclockapi.com/api/json/utc/now' -Method GET).currentFileTime)
+    #Gets Logged In User for In-Place Upgrade Reporting
+    $tsenv.Value('XLoggedInUser') = (Get-CimInstance –ClassName Win32_ComputerSystem | Select-Object UserName -ErrorAction SilentlyContinue).Username 
 }
 if ($StartEnd -eq 'End') {
-    $tsenv.Value('EndTime') = Get-Date
+    $tsenv.Value('EndTime') = [datetime]::FromFileTime((Invoke-RestMethod -Uri 'http://worldclockapi.com/api/json/utc/now' -Method GET).currentFileTime)
 }
