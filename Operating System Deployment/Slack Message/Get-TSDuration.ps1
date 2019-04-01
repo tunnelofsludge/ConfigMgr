130010af5b3b17e2417571d6db197d2cee05db5c
Kuntz <john.kuntz@cmworks.com>
2019-04-01 08:46:37 -0400

    Fixed both conversions to use UTC

--- Operating System Deployment/Slack Message/Get-TSDuration.ps1
+++ Operating System Deployment/Slack Message/Get-TSDuration.ps1
@@ -10,10 +10,10 @@ $tsenv = New-Object -ComObject Microsoft.SMS.TSEnvironment
 
 # Set the Task Sequence Variable based on the parameter passed.
 if ($StartEnd -eq 'Start') {
-    $tsenv.Value('StartTime') = [datetime]::FromFileTime((Invoke-RestMethod -Uri 'http://worldclockapi.com/api/json/utc/now' -Method GET).currentFileTime)
+    $tsenv.Value('StartTime') = [datetime]::FromFileTimeUTC((Invoke-RestMethod -Uri 'http://worldclockapi.com/api/json/utc/now' -Method GET).currentFileTime)
     #Gets Logged In User for In-Place Upgrade Reporting
     $tsenv.Value('XLoggedInUser') = (Get-CimInstance –ClassName Win32_ComputerSystem | Select-Object UserName -ErrorAction SilentlyContinue).Username 
 }
 if ($StartEnd -eq 'End') {
-    $tsenv.Value('EndTime') = [datetime]::FromFileTime((Invoke-RestMethod -Uri 'http://worldclockapi.com/api/json/utc/now' -Method GET).currentFileTime)
+    $tsenv.Value('EndTime') = [datetime]::FromFileTimeUTC((Invoke-RestMethod -Uri 'http://worldclockapi.com/api/json/utc/now' -Method GET).currentFileTime)
 }
