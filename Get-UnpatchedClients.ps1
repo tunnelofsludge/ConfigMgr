e677864cd2d4ed2c4954c8d8776e5e5bf95965db
John Kuntz <johnkuntz1@gmail.com>
2020-02-15 14:59:29 -0500

    Initial Commit

---
+++ Get-UnpatchedClients.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+Gets patch compliance on clients
+
+.DESCRIPTION
+This script checks the date of the last hotfix installed. If the last hotfix installed date is greater than 
+the desired threshold ($lastpatchdays) it returns $false for non-compliance. If there is a hotfix installed
+before the threshold, the script checks to see when the computer was last rebooted. If the last hotfix install date
+is greater than the last reboot, it returns false and is non-compliant. Otherwise the client is patched and rebooted.
+
+A reminder that PATCHING IS REBOOTING!
+
+.NOTES
+2020-02-14 - Initial build. Happy Valentines Day!
+2020-02-15 - Added the check for the last reboot and made the patching threshold a variable 
+#>
+$date = Get-Date
+$lastreboot = (Get-CimInstance -ClassName win32_operatingsystem -ErrorAction Stop | Select-Object lastbootuptime).lastbootuptime
+#Days since last patched
+[int]$lastpatchdays = 60 
+
+try {
+    $lasthotfix = (Get-Hotfix -ErrorAction Stop | Sort-Object InstalledOn | Select-Object -Last 1).InstalledOn
+
+    $timespan = New-TimeSpan -Start $lasthotfix -End $date
+
+    if ($($timespan).Days -gt $lastpatchdays) {
+        #Non Compliant machines should report false
+        $false
+    }
+    elseif ($lasthotfix -gt $lastreboot) {
+        #Patching IS Rebooting!!!!!
+        $false
+    }
+    else {
+        #Computer is Compliant and patched
+        $true
+        
+    }
+}
+catch {
+    Write-Output 'Error Occurred'
+}
